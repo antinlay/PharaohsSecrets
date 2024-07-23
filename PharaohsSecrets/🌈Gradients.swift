5240dc2710b5eb1df854e5ba223e17b0a2899bd0
Janiecee <janiecee@student.21-school.ru>
2024-07-23 21:14:59 +0300

    Сверстаны Treasure для открытия

--- PharaohsSecrets/🌈Gradients.swift
+++ PharaohsSecrets/🌈Gradients.swift
@@ -23,4 +23,53 @@ extension Color {
             endRadius: 50
         )
     }
+    
+    static var randomTreasuryGradient: RadialGradient {
+        [Color.redTreasuryGradient, Color.cyanTreasuryGradient, Color.greenTreasuryGradient, Color.whiteTreasuryGradient, Color.pinkTreasuryGradient].randomElement() ?? Color.whiteTreasuryGradient
+    }
+    
+    static var redTreasuryGradient: RadialGradient {
+        RadialGradient(
+            gradient: Gradient(colors: [Color(#colorLiteral(red: 1, green: 0.179, blue: 0.179, alpha: 1)), .clear]),
+            center: .center,
+            startRadius: 20,
+            endRadius: 65
+        )
+    }
+
+    static var cyanTreasuryGradient: RadialGradient {
+        RadialGradient(
+            gradient: Gradient(colors: [Color(#colorLiteral(red: 0.196, green: 1, blue: 0.952, alpha: 1)), .clear]),
+            center: .center,
+            startRadius: 20,
+            endRadius: 65
+        )
+    }
+    
+    static var greenTreasuryGradient: RadialGradient {
+        RadialGradient(
+            gradient: Gradient(colors: [Color(#colorLiteral(red: 0.26, green: 1, blue: 0, alpha: 1)), .clear]),
+            center: .center,
+            startRadius: 20,
+            endRadius: 65
+        )
+    }
+    
+    static var pinkTreasuryGradient: RadialGradient {
+        RadialGradient(
+            gradient: Gradient(colors: [Color(#colorLiteral(red: 1, green: 0.271, blue: 0.752, alpha: 1)), .clear]),
+            center: .center,
+            startRadius: 20,
+            endRadius: 65
+        )
+    }
+
+    static var whiteTreasuryGradient: RadialGradient {
+        RadialGradient(
+            gradient: Gradient(colors: [Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), .clear]),
+            center: .center,
+            startRadius: 20,
+            endRadius: 65
+        )
+    }
 }
--- PharaohsSecrets/👑Treasury.swift
+++ PharaohsSecrets/👑Treasury.swift
@@ -7,35 +7,114 @@
 
 import SwiftUI
 
+struct TreasuryItem: Hashable {
+    var items: [ImageResource] = [.Treasury.eagle, .Treasury.jugs, .Treasury.mummy, .Treasury.pharaoh, .Treasury.pyramids, .Treasury.upout]
+    var placeholder: ImageResource = .TreasuryView.placeholder
+    var item: ImageResource
+    
+    var isOpen: Bool = false {
+        didSet {
+            if isOpen && !oldValue {
+                self.item = self.items.randomElement() ?? .Treasury.upout
+            }
+        }
+    }
+    
+    var isPlaceHolder: Bool {
+        item == placeholder
+    }
+    
+    init() {
+        self.item = self.placeholder
+    }
+}
+
+struct TreasuryItemView: View {
+    @Binding var item: TreasuryItem
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(Color.randomTreasuryGradient)
+                .blur(radius: 10)
+                .frame(width: 65)
+            Image(item.item)
+                .onTapGesture {
+                    withAnimation {
+                        item.isOpen = true
+                    }
+                }
+        }
+        .animation(.bouncy(duration: 1), value: item.isOpen)
+        .frame(width: 97, height: 94)
+    }
+}
+
 struct Treasury: View {
-    @State private var treasury: [ImageResource] = Array(repeating: .TreasuryView.placeholder, count: 10)
+    @State private var treasury: [TreasuryItem] = Array(repeating: TreasuryItem(), count: 3 * 10)
+    
+    @State private var selectedTab = 0
+    
+    private var indexTabView: some View {
+        HStack(spacing: 14) {
+            ForEach(0..<3) { index in
+                Image(index == selectedTab ? .TreasuryView.current : .TreasuryView.other)
+                    .onTapGesture {
+                        withAnimation {
+                            selectedTab = index
+                        }
+                    }
+            }
+        }
+        .background(.clear)
+        .padding()
+    }
+    
+    private func horizontalGrid(_ range: Range<Int>, _ treasury: Binding<[TreasuryItem]>) -> some View {
+        LazyHGrid(rows: [GridItem(.fixed(100)), GridItem(.fixed(100))], spacing: 14) {
+            ForEach(range, id: \.self) { index in
+                TreasuryItemView(item: $treasury[index])
+            }
+        }
+    }
+    
+    private var title: some View {
+        Text("Treasury")
+            .textCase(.uppercase)
+            .font(.leagueGothic(.regular, size: 56))
+            .foregroundStyle(Color.actionGradient)
+            .frame(maxHeight: .infinity, alignment: .top)
+            .padding(.top)
+    }
+    
+    private var backButton: some View {
+        Image(.TreasuryView.backButton)
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+            .padding(.top, 40)
+            .onTapGesture {
+                // Назад
+            }
+    }
+    
     var body: some View {
         ZStack {
             fullScreenBackground(.Histories.background)
                 .scaleEffect(1.43)
                 .blur(radius: 11, opaque: true)
-            Image(.TreasuryView.backButton)
-                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
-                .padding(.top, 40)
+            backButton
             VStack {
-                Text("Treasury")
-                    .textCase(.uppercase)
-                    .font(.leagueGothic(.regular, size: 56))
-                    .foregroundStyle(Color.actionGradient)
-                    .frame(maxHeight: .infinity, alignment: .top)
-                    .padding(.top)
+                title
                 
-                LazyHGrid(rows: [GridItem(.fixed(100)), GridItem(.fixed(100))], spacing: 14) {
-                    ForEach(treasury.indices, id: \.self) { trea in
-                        Image(treasury[trea])
-                    }
+                TabView(selection: $selectedTab) {
+                    horizontalGrid(0..<10, $treasury).tag(0)
+                    horizontalGrid(10..<20, $treasury).tag(1)
+                    horizontalGrid(20..<30, $treasury).tag(2)
                 }
-                HStack {
-                    Image(.TreasuryView.current)
-                    Image(.TreasuryView.other)
-                    Image(.TreasuryView.other)
-                }
-                .padding()
+                .ignoresSafeArea()
+                .frame(width: .infinity, height: 210)
+                .tabViewStyle(.page(indexDisplayMode: .never))
+                
+                indexTabView
             }
         }
     }
